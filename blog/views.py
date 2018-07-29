b4d14164d4df2b9a4ba176154a3182a536777bb3
wabuluka <dwabuluka@gmail.com>
2018-07-29 14:44:00 +0300

    My django gorls app, first commit

---
+++ blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.template.context_processors import request
+
+# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html',{})
